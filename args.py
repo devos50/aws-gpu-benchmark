af22065bd33bdb620a0ccf3b2eb3c14fb0d20a0b
Martijn de Vos <martijn.devos@epfl.ch>
2024-12-13 13:42:34 +0100

    Reduced default values

--- args.py
+++ args.py
@@ -1,12 +1,12 @@
 import argparse
 
-SUPPORTED_MODELS = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "vit-base-patch16-224", "vit-large-patch16-224", "vit-huge-patch14-224-in21k"]
+SUPPORTED_MODELS = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "vit-base-patch16-224", "vit-large-patch16-224"]
 
 def get_args():
     parser = argparse.ArgumentParser(description='AWS GPU Benchmarking Arguments')
     parser.add_argument('--model', type=str, default="resnet18", help='Name of the model to use', choices=SUPPORTED_MODELS)
-    parser.add_argument('--batch-size', type=int, default=128, help='Batch size for training')
-    parser.add_argument('--num_epochs', type=int, default=10, help='Number of epochs for training')
+    parser.add_argument('--batch-size', type=int, default=64, help='Batch size for training')
+    parser.add_argument('--num_epochs', type=int, default=1, help='Number of epochs for training')
     parser.add_argument('--test-all', action='store_true', help='Test all models')
     
     args = parser.parse_args()
