c656f67be86d65b8080b97793d659a918514081e
Martijn de Vos <mdmartijndevos@gmail.com>
2024-12-20 10:56:46 +0100

    Added various NLP models

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ def get_model(model_name, dataset_name):
     elif dataset_name == "tiny-imagenet":
         num_classes = 200
 
+    # Vision models
     if model_name == "resnet18":
         from torchvision.models import resnet18
         return resnet18(num_classes=num_classes)
@@ -28,9 +29,38 @@ def get_model(model_name, dataset_name):
     elif model_name == "efficientnet-b7":
         from transformers import AutoModelForImageClassification
         return AutoModelForImageClassification.from_pretrained('google/efficientnet-b7')
-    elif model_name == "bert-base-uncased":
-        from transformers import BertForSequenceClassification
-        return BertForSequenceClassification.from_pretrained('bert-base-uncased')
     elif "vit" in model_name:
         from transformers import ViTForImageClassification
         return ViTForImageClassification.from_pretrained('google/%s' % model_name)
+    elif model_name == "densenet121":
+        from torchvision.models import densenet121
+        return densenet121(num_classes=num_classes)
+    elif model_name == "densenet169":
+        from torchvision.models import densenet169
+        return densenet169(num_classes=num_classes)
+    elif model_name == "densenet201":
+        from torchvision.models import densenet201
+        return densenet201(num_classes=num_classes)
+    elif model_name == "densenet161":
+        from torchvision.models import densenet161
+        return densenet161(num_classes=num_classes)
+    
+    # NLP models
+    elif model_name == "bert-base-uncased":
+        from transformers import BertForSequenceClassification
+        return BertForSequenceClassification.from_pretrained('bert-base-uncased')
+    elif model_name in ["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"]:
+        from transformers import GPT2LMHeadModel
+        return GPT2LMHeadModel.from_pretrained(model_name)
+    elif model_name in ["roberta-base", "roberta-large"]:
+        from transformers import RobertaForSequenceClassification
+        return RobertaForSequenceClassification.from_pretrained(model_name)
+    elif model_name in ["distilbert-base-uncased", "distilbert-base-uncased-distilled-squad"]:
+        from transformers import DistilBertForQuestionAnswering
+        return DistilBertForQuestionAnswering.from_pretrained(model_name)
+    elif model_name in ["albert-base-v1", "albert-large-v1"]:
+        from transformers import AlbertForSequenceClassification
+        return AlbertForSequenceClassification.from_pretrained(model_name)
+    elif model_name in ["t5-small", "t5-base", "t5-large", "t5-3b", "t5-11b"]:
+        from transformers import T5ForConditionalGeneration
+        return T5ForConditionalGeneration.from_pretrained(model_name)
--- serialize_to_disk.py
+++ serialize_to_disk.py
@@ -7,7 +7,15 @@ from models import get_model
 import torch
 
 
-MODELS_TO_TEST = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152", "efficientnet-b7", "mobilenet_v3_large", "vit-base-patch16-224", "vit-large-patch16-224", "bert-base-uncased"]
+# MODELS_TO_TEST = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152",
+#                   "efficientnet-b7",
+#                   "mobilenet_v3_large",
+#                   "vit-base-patch16-224", "vit-large-patch16-224",
+#                   "bert-base-uncased",
+#                   "dense121", "dense169", "dense201", "dense161"]
+
+MODELS_TO_TEST = ["gpt2"]
+
 MODEL_PATH = os.path.join("data", "model.pt")
 TIME_FILE_PATH = os.path.join("data", "serialization_times.csv")
 
@@ -25,13 +33,11 @@ def benchmark_serialization_speed(model_name):
     print(f"Testing serialization speed for model {model_name}...")
 
     if os.path.exists(MODEL_PATH):
-        os.remove(MODEL_PATH)
-
-    # Load the model
-    print(f"Loading model {model_name}...")
-    model = get_model(model_name, "cifar10")
+        os.remove(MODEL_PATH)    
 
     for _ in range(10):
+        model = get_model(model_name, "cifar10")
+
         # Serialize the model
         start_time = time.time()
         model_data = torch.save(model.state_dict(), MODEL_PATH)
@@ -50,6 +56,9 @@ def benchmark_serialization_speed(model_name):
         # Log serialization and deserialization times
         with open(TIME_FILE_PATH, "a") as f:
             f.write(f"{model_name},{serialized_size},{serialize_time:.4f},{deserialize_time:.4f}\n")
+
+        del model
+        time.sleep(1)
 
 
 if __name__ == "__main__":
